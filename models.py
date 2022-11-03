143a681c40bfab7f04e68be0345b3d4819479c06
KingAndrew801 <111556315+KingAndrew801@users.noreply.github.com>
2022-11-02 22:10:43 -0600

    CHANGED 1 THING

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pets.db"
+db = SQLAlchemy(app)
 
 class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
